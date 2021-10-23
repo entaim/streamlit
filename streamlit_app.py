6483b8cad163319a2fab18c1c18b442b5921eed2
A.Safarji <a.m.safarji@gmail.com>
2021-10-24 01:17:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,6 @@ This app predicts the **Boston House Price**!
 """)
 st.write('---')
 
-st.session_state['answer'] = ''
-
-st.write(st.session_state)
-
-realans = ['', 'abc', 'edf']
-
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
-
-st.write(st.session_state)
-st.write(answerStat)
-
 
 
 # Loads the Boston House Price Dataset

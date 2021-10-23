057c2475c2b080bce231459cb0fb96245b922875
A.Safarji <a.m.safarji@gmail.com>
2021-10-24 02:00:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ st.write('---')
 
 #boston = load_boston()
 #from sklearn.datasets import load_boston
-boston_dataset = load_boston()
+boston = load_boston()
 #X, Y = boston.data, boston.target
 #print(boston)
-boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
-boston['MEDV'] = boston_dataset.target
+#boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
+#boston['MEDV'] = boston_dataset.target
 # Loads the Boston House Price Dataset
 #boston = "http://lib.stat.cmu.edu/datasets/boston"
 #X = pd.DataFrame(raw_df.data, columns=raw_df.feature_names)

555aeb54abe4ac065e1da1fb320a66d28ae74070
A.Safarji <a.m.safarji@gmail.com>
2021-10-25 01:22:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.write("""
 # Dynamic Price Prediction 
 * This app predicts the **Booking.com Prices**!
 
- ![]https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png
+ 
 
 """)
 st.write('---')
@@ -147,10 +147,10 @@ st.write('---')
 st.write('* Our Client')
 #st.write(x1)
 #st.write()
-t.write("""
+st.write("""
 
 
-
+![]https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png
 
 """)
 

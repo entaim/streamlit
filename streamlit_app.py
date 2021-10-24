8abad68885c1172d3de372a6879386861ca6360c
A.Safarji <a.m.safarji@gmail.com>
2021-10-24 14:41:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,6 +133,7 @@ explainer = shap.TreeExplainer(model)
 shap_values = explainer.shap_values(X)
 
 st.header('Feature Importance')
+st.write('SHAPE values show how much a given feature changed our prediction')
 plt.title('Feature importance based on SHAP values')
 shap.summary_plot(shap_values, X)
 st.pyplot(bbox_inches='tight')

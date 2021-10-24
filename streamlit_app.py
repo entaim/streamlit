5d074309db49fd460b0a39a9fc54fc9f72b2e2da
A.Safarji <a.m.safarji@gmail.com>
2021-10-25 01:18:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,12 +146,13 @@ st.write('* Our Client')
 #st.write(x1)
 #st.write()
 t.write("""
+
 ![](https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png)
 
 
 """)
 
-
+![](https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png)
 
 
 

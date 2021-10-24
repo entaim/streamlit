5c7dda72526bb75fd1e681200326ac8697de7397
A.Safarji <a.m.safarji@gmail.com>
2021-10-24 09:56:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,25 +47,35 @@ def user_input_features():
     #NOX = st.sidebar.slider('NOX',  float(X.NOX.min()),  float(X.NOX.max()),  float(X.NOX.mean()))
     #RM = st.sidebar.slider('RM',  float(X.RM.min()),  float(X.RM.max()),  float(X.RM.mean()))
     #AGE = st.sidebar.slider('AGE',  float(X.AGE.min()),  float(X.AGE.max()),  float(X.AGE.mean()))
-    DIS = st.sidebar.slider('DIS',  float(X.DIS.min()),  float(X.DIS.max()),  float(X.DIS.mean()))
-    RAD = st.sidebar.slider('RAD',  float(X.RAD.min()),  float(X.RAD.max()),  float(X.RAD.mean()))
-    TAX = st.sidebar.slider('TAX',  float(X.TAX.min()),  float(X.TAX.max()),  float(X.TAX.mean()))
-    PTRATIO = st.sidebar.slider('PTRATIO',  float(X.PTRATIO.min()), float(X.PTRATIO.max()),  float(X.PTRATIO.mean()))
-    B = st.sidebar.slider('B',  float(X.B.min()),  float(X.B.max()),  float(X.B.mean()))
-    LSTAT = st.sidebar.slider('LSTAT',  float(X.LSTAT.min()),  float(X.LSTAT.max()),  float(X.LSTAT.mean()))
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
+    beds = st.sidebar.slider('beds',  float(X.beds.min()),  float(X.beds.max()),  float(X.beds.mean()))
+    number_of_ratings = st.sidebar.slider('number_of_ratings',  float(X.number_of_ratings.min()),  float(X.number_of_ratings.max()),  float(X.number_of_ratings.mean()))
+    rating = st.sidebar.slider('rating',  float(X.rating.min()),  float(X.rating.max()),  float(X.rating.mean()))
+    #PTRATIO = st.sidebar.slider('PTRATIO',  float(X.PTRATIO.min()), float(X.PTRATIO.max()),  float(X.PTRATIO.mean()))
+    #B = st.sidebar.slider('B',  float(X.B.min()),  float(X.B.max()),  float(X.B.mean()))
+    #LSTAT = st.sidebar.slider('LSTAT',  float(X.LSTAT.min()),  float(X.LSTAT.max()),  float(X.LSTAT.mean()))
+    data = {'beds': beds,
+            'number_of_ratings': number_of_ratings,
+            'rating': rating}
+            
+            
+          
+        
+            #'LSTAT': LSTAT}
+            #data = {'CRIM': CRIM,
+          #  'ZN': ZN,
+          #  'INDUS': INDUS,
+          #  'CHAS': CHAS,
+           # 'NOX': NOX,
+            #'RM': RM,
+            #'AGE': AGE,
+            #'DIS': DIS,
+            #'RAD': RAD,
+            #'TAX': TAX,
+            #'PTRATIO': PTRATIO,
+            #'B': B,
+            #'LSTAT': LSTAT}
+            
+            
     features = pd.DataFrame(data, index=[0])
     return features
   

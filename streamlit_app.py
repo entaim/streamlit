8bc673c99244ecf581858d46f7398ca3e9e7a46e
A.Safarji <a.m.safarji@gmail.com>
2021-10-24 12:32:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,11 @@ st.write(X)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=10)
 
 X_cv, X_test, y_cv, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=10)
+
+sc = RobustScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
 # Header of Specify Input Parameters
 st.sidebar.header('Specify Input Parameters')
 

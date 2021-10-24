0fb124e6c88fe7326a5884f09f253f93fc9dc48e
A.Safarji <a.m.safarji@gmail.com>
2021-10-25 01:14:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ st.write('---')
 st.write("""
 # Dynamic Price Prediction 
 * This app predicts the **Booking.com Prices**!
+![](https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png)
+
 """)
 st.write('---')
 
@@ -143,7 +145,7 @@ st.pyplot(bbox_inches='tight')
 st.write('---')
 st.write('* Our Client')
 #st.write(x1)
-st.write(https://user-images.githubusercontent.com/20365333/138614995-9351a4ea-b100-42b0-ab69-aa3a02216636.png)
+st.write()
 
 
 

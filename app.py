b16236c90a1caa2f5bbf0c2e6b277abc2128405d
A.Safarji <a.m.safarji@gmail.com>
2021-11-03 00:40:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if uploaded_file is not None:
 else:
 	st.write(input_params)
 
-load_clf=pickle.load(open('dt_saved_07032020.pkl','rb'))
+#load_clf=pickle.load(open('dt_saved_07032020.pkl','rb'))
 #load_clf= pd.read_csv("water1.csv")
 load_clf= load_model('dt_saved_07032020.pkl')
 prediction=load_clf.predict(input_params)

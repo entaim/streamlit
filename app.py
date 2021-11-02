ff037569f416bf82be83f29536e9e68c5c0608b8
A.Safarji <a.m.safarji@gmail.com>
2021-11-02 23:23:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if uploaded_file is not None:
 else:
 	st.write(input_params)
 
-load_clf=pickle.load(open('water.pkl','rb'))
+load_clf=pickle.load(open('water1.csv','rb'))
 
 prediction=load_clf.predict(input_params)
 st.subheader("The Prediction is")

24bf609e2c7a12f628e330785d5887c36cff0593
A.Safarji <a.m.safarji@gmail.com>
2021-11-03 16:46:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ else:
 load_clf= load_model('dt_saved_07032020')
 prediction=load_clf.predict(input_params)
 st.write('---')
-st.subheader("The Prediction is")
+st.subheader(":large_blue_diamond: The Prediction is")
 st.write(":bulb:==",prediction[0])
 
 if(prediction[0]==1):

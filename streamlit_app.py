f34cf9b98dbd210cdc5be0c1b93e4716ddf82b9f
entaim <fnalshaikh@gmail.com>
2021-11-05 22:17:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split  
 import pickle 
 import xgboost as xgb
-import cv as cv2
+import cv2 as cv
 
 # digit_path = 'https://git.uwaterloo.ca/aaljmiai/ahdd1/-/raw/master/'
 # y_test = pd.read_csv(digit_path + 'csvTestLabel_10k_x_1.csv')

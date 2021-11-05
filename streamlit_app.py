aac73ad0577f9c252d3ed0e9d0e44ef29413bd3f
entaim <fnalshaikh@gmail.com>
2021-11-05 19:24:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ X_test = pd.read_csv(digit_path + 'csvTestImages_10k_x_784.csv')
 #y_train = pd.read_csv(digit_path + 'csvTrainLabel_60k_x_1.csv')
 #X_train = pd.read_csv(digit_path + 'csvTrainImages_60k_x_784.csv')
 
-st.write(pd.concat(y_test,X_test)
+st.write(pd.concat(y_test,X_test))
 
 st.write("""
 # Welcome to Booking  Prediction :star: 

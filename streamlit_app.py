a759ab77139413e4e24a961a38491a6627cd57ee
entaim <fnalshaikh@gmail.com>
2021-11-05 19:48:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,15 @@ from sklearn.model_selection import train_test_split
 import pickle 
 
 
-digit_path = 'https://git.uwaterloo.ca/aaljmiai/ahdd1/-/raw/master/'
-y_test = pd.read_csv(digit_path + 'csvTestLabel_10k_x_1.csv')
-X_test = pd.read_csv(digit_path + 'csvTestImages_10k_x_784.csv')
+# digit_path = 'https://git.uwaterloo.ca/aaljmiai/ahdd1/-/raw/master/'
+# y_test = pd.read_csv(digit_path + 'csvTestLabel_10k_x_1.csv')
+# X_test = pd.read_csv(digit_path + 'csvTestImages_10k_x_784.csv')
 #y_train = pd.read_csv(digit_path + 'csvTrainLabel_60k_x_1.csv')
 #X_train = pd.read_csv(digit_path + 'csvTrainImages_60k_x_784.csv')
 
 #dd = 
-st.write(X_test)
-st.code()
+# st.write(X_test)
+# st.code()
 st.success('Success message')
 st.write("""
 # Welcome to Booking  Prediction :star: 

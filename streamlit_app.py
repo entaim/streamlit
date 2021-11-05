f751c0dd4f7bf48d4fd16dbaed5d7eb817743a45
entaim <fnalshaikh@gmail.com>
2021-11-05 19:07:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,23 +28,10 @@ st.write("""
 
 """)
 st.write('---')
-#boston = load_boston()
-#from sklearn.datasets import load_boston
-#boston = load_boston()
-#X, Y = boston.data, boston.target
-#print(boston)
-#boston = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
-#boston['MEDV'] = boston_dataset.target
-# Loads the Boston House Price Dataset
-#boston = "http://lib.stat.cmu.edu/datasets/boston"
-#X = pd.DataFrame(raw_df.data, columns=raw_df.feature_names)
-#Y = pd.DataFrame(raw_df.target, columns=["MEDV"])
-#X = pd.DataFrame(boston.data, columns=boston.feature_names)
-#Y = pd.DataFrame(boston.target, columns=["MEDV"])
+
 
 df4 = pd.read_csv('reg22.csv') 
 x1= df4
-
 x1['Log_price'] = np.log(x1['price'])
 
 #st.write(x1)

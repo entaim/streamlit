04c9a4744da04e6ab9abae9c3072c5556a65e692
entaim <fnalshaikh@gmail.com>
2021-11-06 00:43:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ def ahmed(uploaded_file):
         #st.image(output, use_column_width=True)
         
         #return model_xgb_2.predict(xgb.DMatrix(vec_img))
-        return cv2.resize(vec_img.reshape(8,8), (192, 192), interpolation = cv2.INTER_AREA),  model_xgb_2.predict(xgb.DMatrix(vec_img))
+        return cv2.resize(vec_img.reshape(8,8), (220, 220), interpolation = cv2.INTER_AREA),  model_xgb_2.predict(xgb.DMatrix(vec_img))
   
   
 

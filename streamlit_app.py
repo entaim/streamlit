20f67aaea8fd63cfd450c8eabe88023eb3ec92cd
A.Safarji <a.m.safarji@gmail.com>
2021-11-05 14:04:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ model.fit(X_train, y_train)
 prediction = model.predict(df)
 
 st.header('Predicted Price (Saudi Riyal) :red_circle:')
-st.write(prediction[0], "SR")
+st.write(round(prediction[0], 2),"SR") 
 st.write('---')
 
 # Explaining the model's predictions using SHAP values
